7b951fb4698e380d76717649bdc6b3c9801eaabe
Cyril-777 <kyrillosmaher10@gmail.com>
2023-04-24 14:58:21 -0700

    insertion and selection sorting algorithms added

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm
+ * @list: to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	listint_t *curr, *prev, *next;
+
+	curr = (*list)->next;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		while (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			prev = curr->prev;
+			prev->next = curr->next;
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			curr->prev = prev->prev;
+			curr->next = prev;
+			if (prev->prev != NULL)
+				prev->prev->next = curr;
+			prev->prev = curr;
+
+			if (curr->prev == NULL)
+				*list = curr;
+
+			print_list(*list);
+		}
+		curr = next;
+	}
+}
