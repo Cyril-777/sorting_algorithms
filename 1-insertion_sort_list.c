b30ae2691ae475c44158f513881ca875ff884a1c
Cyril-777 <kyrillosmaher10@gmail.com>
2023-04-24 16:57:08 -0700

    insertion sort list fixed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,40 +1,48 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swap two nodes in a doubly linked list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swap_nodes(listint_t *node1, listint_t *node2)
+{
+	node1->prev->next = node2;
+	node2->prev->next = node1;
+
+	node1->next->prev = node2;
+	node2->next->prev = node1;
+
+	listint_t *temp = node1->prev;
+
+	node1->prev = node2->prev;
+	node2->prev = temp;
+
+	temp = node1->next;
+	node1->next = node2->next;
+	node2->next = temp;
+
+	print_list(*list);
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers in
  * ascending order using the Insertion sort algorithm
  * @list: to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	listint_t *cursor;
+
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
-	listint_t *curr, *prev, *next;
-
-	curr = (*list)->next;
-	while (curr != NULL)
+	cursor = *list;
+	while (cursor->next)
 	{
-		next = curr->next;
-		prev = curr->prev;
-
-		while (curr->prev != NULL && curr->next < curr->prev->next)
-		{
-			prev = curr->prev;
-			prev->next = curr->next;
-			if (curr->next != NULL)
-				curr->next->prev = prev;
-			curr->prev = prev->prev;
-			curr->next = prev;
-			if (prev->prev != NULL)
-				prev->prev->next = curr;
-			prev->prev = curr;
-
-			if (curr->prev == NULL)
-				*list = curr;
-
-			print_list(*list);
-		}
-		curr = next;
+		if (cursor->n > cursor->next->n)
+			swap_nodes(cursor, cursor->next);
+		else
+			cursor = cursor->next;
 	}
 }
